ff3d3e1490668e4215bd469c7fb3e1b95d2e95a7
Ethan Miller <ethanzanemiller@gmail.com>
2012-01-11 08:27:36 +0100

    Submenu featured added to nav menu.

--- templates/topspin-standard/nav-menu.php
+++ templates/topspin-standard/nav-menu.php
@@ -5,23 +5,75 @@ global $store;
 $menu = $store->getSetting('topspin_navmenu');
 $storesListInOrder = $store->getStoresInOrder();
 
+# generate list of store page ids in order
+$storeIDs = array();
+foreach ($storesListInOrder as $item) {
+  array_push($storeIDs, $item->ID);
+}
 ?>
 
 <!-- Only print out menu if the menu is activated -->
 <?php if ($menu == 'on') : ?>
-<div class="topspin-store-navmenu">
-<ul class="topspin-store-navmenu">
-<?php foreach ($storesListInOrder as $item) : ?>
-  <li class="topspin-store-navmenu"><a class="topspin-store-navmenu" href="<?php bloginfo('wpurl'); ?>/<?php echo $item->post_name; ?>/"><?php echo $item->post_title; ?></a></li>
-<?php endforeach; ?>
-</ul>
-</div>
+
+<?php 
+    $args = array(
+        'include'  => $storeIDs,
+        'orderby' => 'post_date',
+        'post_type'=> 'page',
+    );
+
+    /* Get posts according to arguments defined above */
+    $pages = get_posts($args);
+    $pages = array_reverse($pages);
+
+    echo "<ul id='topspin-navmenu'>";
+
+    /* Loop through the array returned by get_posts() */
+    foreach ($pages as $page) {
+
+        /* Grab the page id */
+        $pageId = $page->ID;
+
+        /* Get page title */
+        $title = $page->post_title;
+
+	/* Get page slug */
+	$slug = $page->post_name;
+
+        echo "<li><a href='" . get_bloginfo('url') . "/$slug'>$title</a>";         
+
+        /* Output child pages if any */
+  	$pageKids = get_pages("child_of=".$pageId."&sort_column=menu_order");
+        if ($pageKids) { 
+ 		echo "<ul>";
+		foreach ($pageKids as $kid) { 
+			echo "<li><a href='" . get_bloginfo('url') . "/$kid->post_name'>".$kid->post_title."</a></li>";
+ 		}
+		echo "</ul>";
+	}
+
+	echo "</li>";
+    } 
+    echo "</ul>";
+?>
+
 <script type="text/javascript">
-$(function(){
-   var path = '/' + location.pathname.substring(1);
-   if ( path ) { 
-     $('a[href$="' + path + '"]').attr('id','selected');
-   }
-});
+sfHover = function() {
+	var sfEls = document.getElementById("topspin-navmenu").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+
+
 </script>
+
+
+
 <?php endif; ?>
