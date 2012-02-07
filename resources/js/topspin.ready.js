93124eb97a962633cd644ac169ad4456388195ba
The Uprising Creative <dev@theuprisingcreative.com>
2012-02-07 10:09:59 -0800

    3.3.3.3 js file

--- resources/js/topspin.ready.js
+++ resources/js/topspin.ready.js
@@ -19,7 +19,7 @@ jQuery(function($) {
 			});
 		}
 	};
-	
+
 	var getPid = function(href) {
 		return href.replace(/(^[\w\d\.\/\:_-]*)?(\/)?#!\//,'');
 	};
@@ -32,21 +32,23 @@ jQuery(function($) {
 	}
 
 	$('a.topspin-view-item').live('click',function(e) {
+		e.preventDefault();
 		var pid = getPid($(this).attr('href'));
 		setTimeout(function() { viewProduct(pid); },10);
+		window.location.hash = '#!/'+pid;
 	});
-	
+
 	$('.topspin-view-more-image-pager .topspin-view-more-image-pager-item a').live('click',function(e) {
 		e.preventDefault();
 		var img = $('img',this);
 		var defaultImage  = $('.topspin-view-more-image-default img',$(this).parent().parent().parent().parent());
 		defaultImage.attr('src',img.attr('src'));
 	});
-	
+
 	$('.topspin-view-more-image .topspin-view-more-image-default .topspin-view-more-image-default-cell a').live('click',function(e) {
 		$(this).parent().parent().parent().toggleClass('topspin-view-more-image-zoom');
 	});
-	
+
 	//Pre 3.2
 	$('a.topspin-colorbox').live('click',function(e) {
 		e.preventDefault();
